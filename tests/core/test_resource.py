fdc26adff7762890316ff4676a8b037b6905e80e
Shiladitya Bose <shiladitya.31.z@gmail.com>
2026-03-07 05:53:27 +0530

    refactor(tests): remove redundant implied actions tests

--- tests/core/test_resource.py
+++ tests/core/test_resource.py
@@ -139,22 +139,3 @@ def test_resource_relationship():
     assert File.folder.target is Folder
     assert File.folder.name == "folder"
     assert File.folder.description == "File belongs to Folder"
-
-
-def test_resource_implied_actions():
-    class File(Resource):
-        read = Action()
-        create = Action()
-        read = Action()
-        delete = Action(implies=lambda: [Document.delete])
-
-    class Document(Resource):
-        read = Action(implies=[File.read])
-        create = Action(implies=[File.create])
-        delete = Action(implies=lambda: [File.delete])
-
-    assert Document.read.implied_actions() == frozenset([File.read])
-    assert Document.create.implied_actions() == frozenset([File.create])
-    assert Document.delete.implied_actions() == frozenset([File.delete])
-
-    assert File.delete.implied_actions() == frozenset([Document.delete])
